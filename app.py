2d397321bb3df8439b9dfb7564ae858ebada0613
Mohit Raje <110410869+Mohit-Raje@users.noreply.github.com>
2025-06-25 20:56:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open('label_encoder_gender' , 'rb') as file:
 with open('scaler' , 'rb') as file:
   scaler=pickle.load(file)
   
-st.title('Customer Chrun prediction')
+st.title('BankChurnAI – AI-Powered Bank Customer Retention Tool')
 
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
@@ -29,32 +29,33 @@ num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
 
-input_data = pd.DataFrame({
-    'CreditScore': [credit_score],
-    'Gender': [label_encoder_gender.transform([gender])[0]],
-    'Age': [age],
-    'Tenure': [tenure],
-    'Balance': [balance],
-    'NumOfProducts': [num_of_products],
-    'HasCrCard': [has_cr_card],
-    'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
-})
-
-
-geo_encode = onehot_encoder_geo.transform([[geography]]).toarray()
-geo_encode_df = pd.DataFrame(geo_encode , columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
-input_data_df=pd.DataFrame(input_data)
-
-input_data_df=pd.concat([input_data_df ,geo_encode_df] , axis=1)
-
-input_data_scaled=scaler.transform(input_data_df)
-prediction=model.predict(input_data_scaled)
-prediction_prob=prediction[0][0]
-st.write(prediction_prob)
-if prediction_prob>0.5:
-  st.write('Likey to churn')
-else:
-  st.write('Unlike to churn')
+if st.button('Predict'):
+  input_data = pd.DataFrame({
+      'CreditScore': [credit_score],
+      'Gender': [label_encoder_gender.transform([gender])[0]],
+      'Age': [age],
+      'Tenure': [tenure],
+      'Balance': [balance],
+      'NumOfProducts': [num_of_products],
+      'HasCrCard': [has_cr_card],
+      'IsActiveMember': [is_active_member],
+      'EstimatedSalary': [estimated_salary]
+  })
   
- 
+  
+  geo_encode = onehot_encoder_geo.transform([[geography]]).toarray()
+  geo_encode_df = pd.DataFrame(geo_encode , columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
+  input_data_df=pd.DataFrame(input_data)
+  
+  input_data_df=pd.concat([input_data_df ,geo_encode_df] , axis=1)
+  
+  input_data_scaled=scaler.transform(input_data_df)
+  prediction=model.predict(input_data_scaled)
+  prediction_prob=prediction[0][0]
+  st.write(prediction_prob)
+  if prediction_prob>0.5:
+    st.write('Likey to churn')
+  else:
+    st.write('Unlike to churn')
+    
+   
